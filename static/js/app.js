f4930af129d0213df6fa6bc74947e97e8319ae48
Maria Wong <maria@Marias-MBP.attlocal.net>
2019-05-09 23:33:06 -0400

    cleans code

--- static/js/app.js
+++ static/js/app.js
@@ -7,9 +7,9 @@
 
     function populateTable(dataArr) {
         const tbody = d3.select('tbody');
-        tbody.html("");
+        tbody.html('');
         dataArr.forEach(function (sighting) {
-            const row = tbody.append("tr");
+            const row = tbody.append('tr');
             Object.values(sighting).forEach(function (value) {
                 row.append('td').text(value)
             });
@@ -17,8 +17,8 @@
     }
 
     function updateFilterDict(filter) {
-        let id = filter.property("id");
-        let value = filter.property("value");
+        let id = filter.property('id');
+        let value = filter.property('value');
         if (value) {
             filterDict[id] = value;
         } else {
@@ -34,21 +34,21 @@
         return filteredData;
     }
 
-    d3.selectAll(".form-control").on("input", function () {
+    d3.selectAll('.form-control').on('input', function () {
         d3.event.preventDefault();
         updateFilterDict(d3.select(this));
         let filteredData = filterData();
         populateTable(filteredData);
     });
 
-    d3.selectAll(".sort").on("click", function () {
-        var hasClass = d3.select(this).classed("active-sort");
+    d3.selectAll('.sort').on('click', function () {
+        var hasClass = d3.select(this).classed('active-sort');
         // only sort column if not already sorted by column
         if (!hasClass) {
-            d3.selectAll(".sort").classed("active-sort", false);
-            d3.select(this).classed("active-sort", true);
-            var columnId = d3.select(this).property("parentElement").id
-            var key = columnId.split("-")[0]
+            d3.selectAll('.sort').classed('active-sort', false);
+            d3.select(this).classed('active-sort', true);
+            var columnId = d3.select(this).property('parentElement').id
+            var key = columnId.split('-')[0]
             let sortedData = tableData;
             sortedData.sort((a, b) => (a[key] > b[key]) ? 1 : -1);
             populateTable(sortedData);
