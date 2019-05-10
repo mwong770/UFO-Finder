b3b5af37001da8b0527168290eb85b60c71f7b53
Maria Wong <maria@Marias-MBP.attlocal.net>
2019-05-09 23:06:39 -0400

    adds initial files

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # UFO-Finder
+
+[Check Out UFO-Finder](https://mwong770.github.io/UFO-Finder/)
+
+
+## Table of Contents
+
+[:computer:  Technologies Used](#technologies-used)
+
+[:dvd:  Installation and Usage](#installation)
+
+[:boom:  Features](#features)
+
+[:bust_in_silhouette:  Developer](#developer)
+
+[:email:  Questions or Comments](#questions-or-comments)
+
+
+## <a name="technologies-used"></a> :computer: Technologies Used
+
+* JavaScript
+* D3.js
+* Bootstrap
+* HTML5
+* CSS3
+
+
+## <a name="installation"></a> :dvd: Installation and Usage
+
+* Clone the UFO-Finder repository to your local computer.
+
+
+## <a name="features"></a> :boom: Features
+
+*
+
+
+## <a name="developer"></a> :bust_in_silhouette: Developer
+
+* Maria Wong
+
+
+## <a name="questions-or-comments"></a> :email: Questions or Comments
+
+If you have any questions or comments, feel free to message me on [LinkedIn](https://www.linkedin.com/in/maria-wong/).
+
+ ### Thanks for checking out UFO Finder!
---
+++ static/js/app.js
@@ -0,0 +1,60 @@
+(function () {
+
+    //from data.js
+    const tableData = data;
+
+    let filterDict = {}
+
+    function populateTable(dataArr) {
+        const tbody = d3.select('tbody');
+        tbody.html("");
+        dataArr.forEach(function (sighting) {
+            const row = tbody.append("tr");
+            Object.values(sighting).forEach(function (value) {
+                row.append('td').text(value)
+            });
+        });
+    }
+
+    function updateFilterDict(filter) {
+        let id = filter.property("id");
+        let value = filter.property("value");
+        if (value) {
+            filterDict[id] = value;
+        } else {
+            delete filterDict[id]
+        }
+    }
+
+    function filterData() {
+        let filteredData = tableData;
+        Object.entries(filterDict).forEach(([key, value]) => {
+            filteredData = filteredData.filter(sighting => sighting[key] === value);
+        });
+        return filteredData;
+    }
+
+    d3.selectAll(".form-control").on("input", function () {
+        d3.event.preventDefault();
+        updateFilterDict(d3.select(this));
+        let filteredData = filterData();
+        populateTable(filteredData);
+    });
+
+    d3.selectAll(".sort").on("click", function () {
+        var hasClass = d3.select(this).classed("active-sort");
+        // only sort column if not already sorted by column
+        if (!hasClass) {
+            d3.selectAll(".sort").classed("active-sort", false);
+            d3.select(this).classed("active-sort", true);
+            var columnId = d3.select(this).property("parentElement").id
+            var key = columnId.split("-")[0]
+            let sortedData = tableData;
+            sortedData.sort((a, b) => (a[key] > b[key]) ? 1 : -1);
+            populateTable(sortedData);
+        }
+    });
+
+    populateTable(tableData);
+
+}());
